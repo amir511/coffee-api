b1691a7be550a18c93aa3b83e1e04b177c8e43ca
Amir Anwar <amir.anwar.said@gmail.com>
2021-02-18 08:45:14 +0200

    Add filtering to views

--- coffee/apps/product/views.py
+++ coffee/apps/product/views.py
@@ -7,8 +7,18 @@ from coffee.apps.product.serializers import CoffeeMachineSerializer, CoffeePodSe
 class CoffeeMachineView(ListAPIView):
     serializer_class = CoffeeMachineSerializer
     queryset = CoffeeMachine.objects.all()
+    filterset_fields = [
+        "product_type",
+        "water_line_compatible",
+        "model_type",
+    ]
 
 
 class CoffeePodView(ListAPIView):
     serializer_class = CoffeePodSerializer
     queryset = CoffeePod.objects.all()
+    filterset_fields = [
+        "product_type",
+        "coffee_flavor",
+        "pack_size",
+    ]
