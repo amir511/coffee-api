232f26863e4931d64e6a0286e07f5da2afe8b2ce
Amir Anwar <amir.anwar.said@gmail.com>
2021-02-18 08:45:46 +0200

    Change model choices to be more sensible

--- coffee/apps/product/models.py
+++ coffee/apps/product/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 class CoffeeMachine(models.Model):
     class ProductType(models.TextChoices):
-        COFFEE_MACHINE_LARGE = "coffe_machine_large", "Coffee machine large"
-        COFFEE_MACHINE_SMALL = "coffe_machine_small", "Coffee machine small"
-        ESPRESSO_MACHINE = "espresso_machine", "Espresso machine"
+        COFFEE_MACHINE_LARGE = "large", "Coffee machine large"
+        COFFEE_MACHINE_SMALL = "small", "Coffee machine small"
+        ESPRESSO_MACHINE = "espresso", "Espresso machine"
 
     class ModelType(models.TextChoices):
-        BASE_MODEL = "base_model", "Base model"
-        PREMIUM_MODEL = "premium_model", "Premium model"
-        DELUXE_MODEL = "deluxe_model", "Deluxe model"
+        BASE_MODEL = "base", "Base"
+        PREMIUM_MODEL = "premium", "Premium"
+        DELUXE_MODEL = "deluxe", "Deluxe"
 
-    SKU_number = models.CharField(max_length=6)
+    SKU_number = models.CharField(max_length=6, unique=True)
     product_type = models.CharField(choices=ProductType.choices, max_length=20)
     water_line_compatible = models.BooleanField()
     model_type = models.CharField(choices=ModelType.choices, max_length=15)
@@ -23,24 +23,24 @@ class CoffeeMachine(models.Model):
 
 class CoffeePod(models.Model):
     class ProductType(models.TextChoices):
-        COFFEE_POD_LARGE = "coffe_pod_large", "Coffee pod large"
-        COFFEE_POD_SMALL = "coffe_pod_small", "Coffee pod small"
-        ESPRESSO_POD = "espresso_pod", "Espresso pod"
+        COFFEE_POD_LARGE = "large", "Coffee pod large"
+        COFFEE_POD_SMALL = "small", "Coffee pod small"
+        ESPRESSO_POD = "espresso", "Espresso pod"
 
     class CoffeeFlavor(models.TextChoices):
-        COFFEE_FLAVOR_VANILLA = "coffee_flavor_vanilla", "Coffee flavor vanilla"
-        COFFEE_FLAVOR_CARAMEL = "coffee_flavor_caramel", "Coffee flavor caramel"
-        COFFEE_FLAVOR_PSL = "coffee_flavor_psl", "Coffee flavor psl"
-        COFFEE_FLAVOR_MOCHA = "coffee_flavor_mocha", "Coffee flavor mocha"
-        COFFEE_FLAVOR_HAZELNUT = "coffee_flavor_hazelnut", "Coffee flavor hazelnut"
+        COFFEE_FLAVOR_VANILLA = "vanilla", "Vanilla"
+        COFFEE_FLAVOR_CARAMEL = "caramel", "Caramel"
+        COFFEE_FLAVOR_PSL = "psl", "Psl"
+        COFFEE_FLAVOR_MOCHA = "mocha", "Mocha"
+        COFFEE_FLAVOR_HAZELNUT = "hazelnut", "Hazelnut"
 
     class PackSize(models.TextChoices):
-        ONE_DOZEN = "one_dozen", "One dozen"
-        THREE_DOZEN = "three_dozen", "Three dozen"
-        FIVE_DOZEN = "five_dozen", "Five dozen"
-        SEVEN_DOZEN = "seven_dozen", "Seven dozen"
+        ONE_DOZEN = "1", "One dozen"
+        THREE_DOZEN = "3", "Three dozen"
+        FIVE_DOZEN = "5", "Five dozen"
+        SEVEN_DOZEN = "7", "Seven dozen"
 
-    SKU_number = models.CharField(max_length=6)
+    SKU_number = models.CharField(max_length=6, unique=True)
     product_type = models.CharField(choices=ProductType.choices, max_length=20)
     coffee_flavor = models.CharField(choices=CoffeeFlavor.choices, max_length=25)
     pack_size = models.CharField(choices=PackSize.choices, max_length=12)
